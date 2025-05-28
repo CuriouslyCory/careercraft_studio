920c4f6859dafcae50d75c570defa2a67a1db36b
CuriouslyCory <cory@hau.me>
2025-05-27 21:17:35 -0700

    Fix url on sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { type MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://resume-master.curiouslycory.com";
+  const baseUrl = "https://careercraft.studio";
 
   return [
     {
